5317b7d13a1953a9290a8605ae643c647d6fcd5e
Amree Zaid <mohd.amree@gmail.com>
2024-10-13 17:28:43 +0800

    Add Alpine.js using importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import 'alpine-turbo-drive-adapter'
+import Alpine from 'alpinejs'
+
+// Initialize Alpine.js
+window.Alpine = Alpine
+Alpine.start()
+
 import "controllers"
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "alpinejs" # @3.14.1
+pin "alpine-turbo-drive-adapter" # @2.1.0
 pin_all_from "app/javascript/controllers", under: "controllers"
